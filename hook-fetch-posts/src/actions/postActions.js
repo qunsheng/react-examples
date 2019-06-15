22fb5e41ee0bc9585c3c335281905725eb29ca8f
Quentin Chen <qunsheng.chen@hotmail.com>
2019-06-15 13:41:24 -0400

    add action files

---
+++ hook-fetch-posts/src/actions/postActions.js
@@ -0,0 +1,40 @@
+// put a list of what to import into import {...}
+import { FETCH_POSTS, NEW_POST } from "./types";
+
+export const fetchPosts = () => {
+  console.log("================ fetch posts");
+};
+
+// export const fetchPosts = () => dispatch => {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then(res => res.json())
+//     .then(posts =>
+//       // dispatch after fetch data
+//       dispatch({
+//         type: FETCH_POSTS,
+//         payload: posts
+//       })
+//     );
+// };
+
+export const createPost = postData => {
+  console.log("==================== create post: ", postData);
+};
+
+// export const createPost = postData => dispatch => {
+//   fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     headers: {
+//       "content-type": "application/json"
+//     },
+//     body: JSON.stringify(postData)
+//   })
+//     .then(res => res.json())
+//     .then(post =>
+//       // dispatch after fetch data
+//       dispatch({
+//         type: NEW_POST,
+//         payload: post
+//       })
+//     );
+// };
--- hook-fetch-posts/src/components/Postform.js
+++ hook-fetch-posts/src/components/Postform.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { createPost } from "../actions/postActions";
+
 export default function Postform() {
   return <div>Post form</div>;
 }
