60e64a40d15329791a2b7de019dc48b32eddbc66
Quentin Chen <qunsheng.chen@hotmail.com>
2019-07-10 22:21:43 -0400

    clean up

--- hook-fetch-posts/src/App.js
+++ hook-fetch-posts/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import Store from "./Store";
@@ -11,11 +10,6 @@ function App() {
   // return jsx
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-
       <Store>
         <PostForm />
         <Posts />
